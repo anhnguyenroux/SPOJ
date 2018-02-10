33d87c055d737af352800e7761676464895ac07f
hlly <vanhop3499@gmail.com>
2018-02-10 17:23:29 +0700

    MAXARR1

---
+++ MAXARR1.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+const int NMAX = 1e5;
+int a[NMAX + 100];
+int f[NMAX + 100];
+void PreCal() {
+    a[0] = 0;
+    a[1] = 1;
+    for (int i = 2; i <=NMAX; i++)
+        if (i % 2 == 0) a[i] = a[i / 2];
+        else a[i] = a[i / 2] + a[i / 2 + 1];
+    f[0] = 0;
+    for (int i = 1; i <= NMAX; i++)
+        f[i] = max(f[i - 1], a[i]);
+}
+void Cal() {
+    int Test;
+    scanf("%d", &Test);
+    while (Test > 0) {
+        int n;
+        scanf("%d", &n);
+        printf("%d\n", f[n]);
+        Test--;
+    }
+}
+int main() {
+    PreCal();
+    Cal();
+    return 0;
+}
