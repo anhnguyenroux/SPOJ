d0b68cd39af3bd83d648b8d5d9c4d364dc198c43
hlly <vanhop3499@gmail.com>
2018-03-04 14:51:04 +0700

    ROBOCON

--- ROBOCON.cpp
+++ ROBOCON.cpp
@@ -1,5 +1,10 @@
+/*
+    ROBOCON : http://vn.spoj.com/problems/ROBOCON/
+    Explanation and Complexity is below
+*/
 #include <stdio.h>
 #include <queue>
+#include <vector>
 using namespace std;
 const int NMAX      =   510;
 const int oo        =   1e9;
@@ -9,21 +14,22 @@ const int k1x[3]    =   {0, 1, 1};
 const int k1y[3]    =   {-1, -1, 0};
 struct node {
     int u, v;
-    int turn;
-    node(int _u, int _v, int _turn) {
+    node(int _u, int _v) {
         u = _u;
         v = _v;
-        turn = _turn;
     }
 };
 int N, k;
 int a[NMAX][NMAX];
-int f[NMAX][NMAX][2];
+int f0[NMAX][NMAX], f1[NMAX][NMAX];
 void readInp() {
     scanf("%d %d", &N, &k);
     for (int i = 1; i <= N; i++)
-        for (int j = 1; j <= N; j++)
+        for (int j = 1; j <= N; j++) {
             a[i][j] = 1;
+            f0[i][j] = 0;
+            f1[i][j] = 0;
+        }
     for (int i = 1; i <= k; i++) {
         int x, y;
         scanf("%d %d", &x, &y);
@@ -31,49 +37,57 @@ void readInp() {
     }
 }
 void bfs() {
-    for (int u = 1; u <= N; u++)
-        for (int v = 1; v <= N; v++) {
-            f[u][v][0] = oo;
-            f[u][v][1] = oo;
-        }
     queue <node> q;
-    node r1 = node(1, 1, 0);
-    node r2 = node(1, N, 1);
-    q.push(r1);
-    q.push(r2);
-    f[1][1][0] = 0;
-    f[1][N][1] = 0;
+    int d1 = 1, d2 = 1;
+    q.push(node(1, 1));
+    q.push(node(1, N));
+    int t = 0;
     while (!q.empty()) {
-        node cur = q.front();
-        q.pop();
-        int u = cur.u, v = cur.v;
-        if (f[u][v][0] == f[u][v][1]) {
-            printf("%d", f[u][v][0]);
-            return;
-        }
-        int turn = cur.turn;
-        if (!turn) {
+        t++;
+        int cnt = 0;
+        for (int i = 1; i <= d1; i++) {
+            node cur = q.front();
+            q.pop();
+            int u = cur.u;
+            int v = cur.v;
             for (int k = 0; k < 3; k++) {
                 int ux = u + k0x[k];
                 int vy = v + k0y[k];
                 if (ux < 1 || ux > N || vy < 1 || vy > N) continue;
-                if (a[ux][vy] && f[ux][vy][0] == oo) {
-                    f[ux][vy][0] = f[u][v][0] + 1;
-                    q.push(node(ux, vy, 0));
+                if (a[ux][vy]) {
+                    if (f0[ux][vy] == 0 || f0[ux][vy] != t) {
+                        q.push(node(ux, vy));
+                        f0[ux][vy] = t;
+                        cnt++;
+                    }
                 }
             }
         }
-        if (turn) {
+        d1 = cnt; 
+        cnt = 0;
+        for (int i = 1; i <= d2; i++) {
+            node cur = q.front();
+            q.pop();
+            int u = cur.u;
+            int v = cur.v;
             for (int k = 0; k < 3; k++) {
                 int ux = u + k1x[k];
                 int vy = v + k1y[k];
                 if (ux < 1 || ux > N || vy < 1 || vy > N) continue;
-                if (a[ux][vy] && f[ux][vy][1] == oo) {
-                    f[ux][vy][1] = f[u][v][1] + 1;
-                    q.push(node(ux, vy, 1));
+                if (a[ux][vy]) {
+                    if (f0[ux][vy] == t) {
+                        printf("%d", t);
+                        return;
+                    }
+                    if (f1[ux][vy] == 0 || f1[ux][vy] != t) {
+                        q.push(node(ux, vy));
+                        f1[ux][vy] = t;
+                        cnt++;
+                    }
                 }
             }
         }
+        d2 = cnt;
     }
 }
 int main() {
@@ -81,3 +95,8 @@ int main() {
     bfs();
     return 0;
 }
+/*
+    We will use BFS - algorithm in this case.
+    For each step, we will find positions that can go from recent position in Queue.
+    When 2 robots meet at time in the same position, we stop BFS and output the time we count for each time.
+*/
