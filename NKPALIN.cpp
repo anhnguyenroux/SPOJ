3f877cf1ebcfa03046059081755bb61747d136ce
hlly <vanhop3499@gmail.com>
2018-03-10 20:32:41 +0700

    NKPALIN

---
+++ NKPALIN.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+using namespace std;
+const int LMAX      =   2100;
+char s[LMAX];
+int n;
+int f[LMAX][LMAX];
+char kq[LMAX];
+void process() {
+    gets(s);
+    n = strlen(s);
+    for (int i = 1; i <= n; i++)
+        f[i][i] = 1;
+    for (int j = 1; j <= n; j++)
+        for (int i = j - 1; i >= 1; i--)
+            if (s[i - 1] == s[j - 1]) f[i][j] = f[i + 1][j - 1] + 2;
+            else 
+                f[i][j] = max(f[i + 1][j], f[i][j - 1]);
+    int i = 1, j = n;
+    int p1 = 1, p2 = f[1][n];
+    while (i <= j) {
+        if (s[i - 1] == s[j - 1]) {
+            kq[p1++] = s[i - 1];
+            kq[p2--] = s[i - 1];
+            i++;
+            j--;
+        }
+        else {
+            if (f[i][j] == f[i + 1][j]) i++;
+            else j--;
+        }
+    }
+    for (int i = 1; i <= f[1][n]; i++)
+        printf("%c", kq[i]);
+}
+int main() {
+    process();
+    return 0;
+}
