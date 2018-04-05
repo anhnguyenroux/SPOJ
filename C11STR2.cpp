f54cbe6e8d41c758505c00a61b97aab06fb35a34
hlly <vanhop3499@gmail.com>
2018-04-05 11:51:26 +0700

    C11STR2 with KMP

---
+++ C11STR2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+const int NMAX      =   1e5 + 10;
+string a, b;
+int n;
+int nxt[2 * NMAX];
+int main() {
+    cin >> a;
+    cin >> b;
+    string s = b + "#" + a;
+    nxt[0] = 0;
+    int j = 0, i = 1;
+    int n = s.length();
+    while (i < n) {
+        while (j > 0 && s[j] != s[i]) {
+            j = nxt[j - 1];
+        }
+        if (s[j] == s[i]) {
+            nxt[i] = ++j;
+            i++;
+        }
+        else {
+            nxt[i] = 0;
+            i++;
+        }
+    }
+    
+    int pos = 0;
+    for (int i = b.length(); i < n; i++)
+        pos = max(pos, nxt[i]);
+    cout << a + b.substr(pos, b.length() - pos);
+    return 0;
+}
